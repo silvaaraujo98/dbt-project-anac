7319610545b94fdd6bb73c0db97d48ab456fb7c6
silvaaraujo98 <jvsilvaaraujo98@gmail.com>
2026-07-04 14:18:32 -0300

    config: inserting milestones to calculate time btw tasks

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from utils import anac_extractor
 from utils import bq_loader
 import logging 
+import timeit
 
 year = "2025"
-month = "10"
+month = "12"
 
 logger = logging.getLogger("ANAC ETL")
 
 # Configure the root logger to accept INFO levels and above
 logging.basicConfig(level=logging.INFO)
-
-logger.info("Start to extract the data from ANAC")
+extraction_start = timeit.default_timer()
+logger.info(f"Start to extract the data from ANAC{month}-{year}")
 
 anac_extractor  = anac_extractor.AnacExtractor(year,month)
 clean_text = anac_extractor.extract_data()
-logger.info("Finished the extraction")
+extraction_end = timeit.default_timer()
+logger.info(f"Finished the extraction and the execution time was: {extraction_end - extraction_start:.3f} secs")
 
+ingestion_gcs_start = timeit.default_timer()
 logger.info("Starting the ingestion to GCS")
 gcp_ingester = bq_loader.GCPDataIngester("data-core-platform")
 gcp_ingester.upload_to_gcs("anac-landing-zone",f"airplane_movements_{year}{month}.csv",clean_text)
-logger.info("Finished the ingestion to GCS")
+ingestion_gcs_end = timeit.default_timer()
+logger.info(f"Finished the ingestion to GCS and the ingestion time was: {ingestion_gcs_end - ingestion_gcs_start:.3f} secs")
 
+ingestion_bq_start = timeit.default_timer()
 logger.info("Starting the ingestion to BigQuery")
 gcp_ingester.load_gcs_to_bigquery("gs://anac-landing-zone/airplane_movements_*.csv","anac_source","movements_raw")
-logger.info("Finished the ingestion to BigQuery")
+ingestion_bq_end = timeit.default_timer()
+logger.info(f"Finished the ingestion to BigQuery and the ingestion time was: {ingestion_bq_end - ingestion_bq_start:.3f} secs")
+logger.info(f"All the process taked {ingestion_bq_end - extraction_start:.3f} secs")
