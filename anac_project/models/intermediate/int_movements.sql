e18ae01de03ade50196ffecbbae3c49d5139f2b3
silvaaraujo98 <jvsilvaaraujo98@gmail.com>
2026-07-10 14:58:40 -0300

    feat: inserting join btw dim_servicetype and stg_movements

--- anac_project/models/intermediate/int_movements.sql
+++ anac_project/models/intermediate/int_movements.sql
@@ -4,10 +4,16 @@ with stg_movements AS (
 
 stg_dim_airports_brazil AS (
     select * from {{ ref('stg_dim_airports_brazil') }}
-),
+)
 
-stg_dim_aircrafts AS (
-    select * from {{ ref('stg_dim_aircrafts') }})
+select 
+    * 
+from 
+    stg_movements 
+left join 
+    stg_dim_airports_brazil 
+    on stg_movements.airport_reference_code = stg_dim_airports_brazil.icao_code_airport
+left join
+    stg_dim_service_type
+    on stg_movements.service_type_code = stg_dim_service_type.service_type_code
 
-select * from stg_movements left join stg_dim_airports_brazil on stg_movements.airport_reference_code = stg_dim_airports_brazil.icao_code_airport
-left join stg_dim_aircrafts on stg_movements.aircraft_registration = stg_dim_aircrafts.icao_code_aircraft
