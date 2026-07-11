3f4351b732d1685fe314af1ff73ba0fa12c47778
silvaaraujo98 <jvsilvaaraujo98@gmail.com>
2026-07-11 16:18:08 -0300

    feat: creating the stg dim service type model

--- anac_project/models/staging/stg_dim_service_type.sql
+++ anac_project/models/staging/stg_dim_service_type.sql
@@ -1,10 +1,10 @@
 with source as (
-    select * from {{source('anac_source','dim_servicetype')}}
+    select * from {{source('anac_source','dim_service_type')}}
 ),
 
 renamed_and_casted as (
     select 
-        trim(service_type_code) as service_type_code,
+        regexp_replace(service_type_code,r'[^a-zA-Z0-9]', '') as service_type_code,
         trim(application) as application,
         trim(type_operation) as type_operation,
         trim(service_type_description) as service_type_description
