341c379f008b9d065fb8274e6ccbe1718cb48e60
silvaaraujo98 <jvsilvaaraujo98@gmail.com>
2026-07-11 16:16:33 -0300

    feat: indicating the stg_service_type in int_movements

--- anac_project/models/intermediate/int_movements.sql
+++ anac_project/models/intermediate/int_movements.sql
@@ -4,16 +4,26 @@ with stg_movements AS (
 
 stg_dim_airports_brazil AS (
     select * from {{ ref('stg_dim_airports_brazil') }}
+),
+
+stg_dim_service_type AS (
+    select * from {{ ref('stg_dim_service_type') }}
 )
 
 select 
-    * 
+    a.*,
+    b.usual_name,
+    b.oficial_name,
+    b.localization,
+    c.application,
+    c.type_operation,
+    c.service_type_description
 from 
-    stg_movements 
+    stg_movements as a
 left join 
-    stg_dim_airports_brazil 
-    on stg_movements.airport_reference_code = stg_dim_airports_brazil.icao_code_airport
+    stg_dim_airports_brazil as b
+    on a.airport_reference_code = b.icao_code_airport
 left join
-    stg_dim_service_type
-    on stg_movements.service_type_code = stg_dim_service_type.service_type_code
+    stg_dim_service_type as c
+    on a.service_type_code = c.service_type_code
 
